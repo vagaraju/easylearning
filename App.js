95462d10ec684e67b862e213d63b87bcd0e29b2c
Vagaraju Gudeseva <vagaraju.gudeseva@openedgepay.com>
2018-01-13 12:01:03 -0700

    First commit App.js file to the project

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  StatusBar 
+} from 'react-native';
+
+
+import Routes from './src/Routes';
+import firebase from 'firebase';
+
+export default class App extends Component<{}> {
+
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyBX_O_SzQLjN2ql-O7GlBAgBA6fmfUVC2w",
+        authDomain: "easy-69639.firebaseapp.com",
+        databaseURL: "https://easy-69639.firebaseio.com",
+        projectId: "easy-69639",
+        storageBucket: "easy-69639.appspot.com",
+        messagingSenderId: "392550874481"
+    });
+}
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+           backgroundColor="#1c313a"
+           barStyle="light-content"
+         />
+        <Routes/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+  }
+});
